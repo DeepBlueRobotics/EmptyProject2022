c69975af2ae181047961914c5cbc3a76b5e3b482
nick-chen4 <92063397+nick-chen4@users.noreply.github.com>
2023-01-17 17:22:04 -0800

    change back

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,7 +19,6 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
   private CANSparkMax leftMaster = new CANSparkMax(); 
-  //CHANGE
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
